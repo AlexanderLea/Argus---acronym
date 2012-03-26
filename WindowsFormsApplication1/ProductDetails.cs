c285386f1468c32d30665817ed9aa432cdefc967
JakeChampion <jakechampion.jake2@googlemail.com>
2012-03-26 13:09:39 +0100

    Added the solution files

---
+++ WindowsFormsApplication1/ProductDetails.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class ProductDetails : Form
+    {
+        public ProductDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, EventArgs e)
+        {
+            MainBranch MainBranch = new MainBranch();
+            MainBranch.Show();
+            this.Hide();
+        }
+    }
+}
